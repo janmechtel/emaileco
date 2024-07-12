8b550f105df8e8584c139f16795d3faf6ce28895
Jan Mechtel <jmechtel@posteo.de>
2024-07-12 23:44:35 +0200

    marimo notebook to download email headers via IMAP

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # emaileco
-Utilities to delete emails from your gmail
+Utilities to delete emails from your Gmail via Imap.
+
+For now there is a [marimo notebook](https://github.com/marimo-team/marimo) that downloads some header information of all emails and stores it as a csv.
+
+Ideas/Roadmap:
+
+- [ ] only use pure emails for To/From
+- [ ] Table view
+- [ ] Sort & Filter
+- [ ] Visuzalize as treemap and possible sunburst chart
+- [ ] ability to bulk delete emails
+
+
---
+++ fetchEmails.py
@@ -0,0 +1,106 @@
+import marimo
+
+__generated_with = "0.7.3"
+app = marimo.App()
+
+
+@app.cell
+def __():
+    import marimo as mo
+    return mo,
+
+
+@app.cell
+def __(mo):
+    mo.md(r"Setup a [Gmail app password](https://support.google.com/accounts/answer/185833?sjid=10666141560394438051-EU)")
+    return
+
+
+@app.cell
+def __(mo):
+    email_address = mo.ui.text(value="jmechtel@gmail.com", label="email")
+    password = mo.ui.text(
+        value="XXX", label="password", kind="password"
+    )
+    mo.vstack([email_address, password])
+    return email_address, password
+
+
+@app.cell
+def __(email_address, password):
+    import imaplib
+    import email
+
+    # Gmail IMAP server and port
+    imap_server = "imap.gmail.com"
+    imap_port = 993
+
+    # Connect to the IMAP server
+    imap = imaplib.IMAP4_SSL(imap_server, imap_port)
+
+    # Login to your Gmail account
+    imap.login(email_address.value, password.value)
+
+    # Select the inbox
+    imap.select("INBOX")
+    return email, imap, imap_port, imap_server, imaplib
+
+
+@app.cell
+def __(imap):
+    resp, data = imap.uid(
+        "FETCH",
+        "1:*",
+        "(BODY.PEEK[HEADER.FIELDS (Date From To Subject)] RFC822.SIZE X-GM-MSGID)",
+    )
+
+    x = 0
+    messages = []
+    for d in data:
+        if len(d) == 2:
+            _msg = {}
+            line1 = d[0].decode().replace("(X-GM-MSGID", "X-GM-MSGID")
+            # print(line1)
+            splits1 = line1.split(" ")
+            for z in range(1, len(splits1) - 1, 2):
+                if splits1[z] in ("X-GM-MSGID", "RFC822.SIZE"):
+                    _msg[splits1[z]] = splits1[z + 1]
+            # print(splits1)
+            line2 = d[1].decode()
+            # print(line2)
+            splits2 = line2.split("\r\n")
+            for s in splits2:
+                if s.startswith("To"):
+                    _msg["To"] = s.replace("To: ", "")
+                elif s.startswith("From"):
+                    _msg["From"] = s.replace("From: ", "")
+                elif s.startswith("Subject"):
+                    _msg["Subject"] = s.replace("Subject: ", "")
+                elif s.startswith("Date"):
+                    _msg["Date"] = s.replace("Date: ", "")
+            messages.append(_msg)
+
+    # Logout from the IMAP server
+    print(messages[0])
+    print(len(messages))
+    imap.logout()
+    return d, data, line1, line2, messages, resp, s, splits1, splits2, x, z
+
+
+@app.cell
+def __():
+    import pandas as pd
+    return pd,
+
+
+@app.cell
+def __(messages, pd):
+    df = pd.DataFrame(messages)
+
+    # Save the DataFrame to a CSV file
+    df.to_csv("emails.csv", index=False)
+    return df,
+
+
+if __name__ == "__main__":
+    app.run()
