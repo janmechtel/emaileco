048b6ad7d765fe5f98ae645128abcd5408385a3a
Jan Mechtel <jmechtel@posteo.de>
2024-07-16 00:31:06 +0200

    Only use pure emails for To/From + some grouping/sorting

--- README.md
+++ README.md
@@ -8,8 +8,11 @@ Ideas/Roadmap:
 - [X] Fetch all emails
 - [X] Table view
 - [X] Sort & Filter
-- [ ] Only use pure emails for To/From
+- [X] Only use pure emails for To/From
 - [ ] Visuzalize as treemap and possible sunburst chart
 - [ ] Ability to bulk delete emails
+- [ ] Store/Restore from local .csv
+- [ ] Test sharing "as app"
+- [ ] deal with NaN rows, maybe UTF encoding? Some To&From adresses are empty
 
 
--- emails.py
+++ emails.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.7.3"
+__generated_with = "0.7.5"
 app = marimo.App(width="full")
 
 
@@ -27,7 +27,7 @@ def __(mo):
 
 
 @app.cell
-def __():
+def __(email_address, password, pd):
     import imaplib
     import email
 
@@ -37,56 +37,63 @@ def __():
 
     # Connect to the IMAP server
     imap = imaplib.IMAP4_SSL(imap_server, imap_port)
-    return email, imap, imap_port, imap_server, imaplib
+
+
+    def getEmailsFromServer():
+        # Login to your Gmail account
+        imap.login(email_address.value, password.value)
+
+        # Select the inbox
+        imap.select("INBOX")
+
+        resp, data = imap.uid(
+            "FETCH",
+            "1:*",
+            "(BODY.PEEK[HEADER.FIELDS (Date From To Subject)] RFC822.SIZE X-GM-MSGID)",
+        )
+
+        x = 0
+        messages = []
+        for d in data:
+            if len(d) == 2:
+                _msg = {}
+                line1 = d[0].decode().replace("(X-GM-MSGID", "X-GM-MSGID")
+                # print(line1)
+                splits1 = line1.split(" ")
+                for z in range(1, len(splits1) - 1, 2):
+                    if splits1[z] == "X-GM-MSGID":
+                        _msg["X-GM-MSGID"] = splits1[z + 1]
+                    elif splits1[z] == "RFC822.SIZE":
+                        _msg["Size"] = int(splits1[z + 1])
+                # print(splits1)
+                line2 = d[1].decode()
+                # print(line2)
+                splits2 = line2.split("\r\n")
+                for s in splits2:
+                    if s.startswith("To"):
+                        _msg["To"] = s.replace("To: ", "")
+                    elif s.startswith("From"):
+                        _msg["From"] = s.replace("From: ", "")
+                    elif s.startswith("Subject"):
+                        _msg["Subject"] = s.replace("Subject: ", "")
+                    elif s.startswith("Date"):
+                        _msg["Date"] = s.replace("Date: ", "")
+                messages.append(_msg)
+
+        # Logout from the IMAP server
+        print(messages[0])
+        print(len(messages))
+        imap.logout()
+        df = pd.DataFrame(messages)
+        df["Date"] = pd.to_datetime(df["Date"], errors="coerce", utc=True)
+        return df
+    return email, getEmailsFromServer, imap, imap_port, imap_server, imaplib
 
 
 @app.cell
-def __(email_address, imap, password):
-    # Login to your Gmail account
-    imap.login(email_address.value, password.value)
-
-    # Select the inbox
-    imap.select("INBOX")
-
-    resp, data = imap.uid(
-        "FETCH",
-        "1:*",
-        "(BODY.PEEK[HEADER.FIELDS (Date From To Subject)] RFC822.SIZE X-GM-MSGID)",
-    )
-
-    x = 0
-    messages = []
-    for d in data:
-        if len(d) == 2:
-            _msg = {}
-            line1 = d[0].decode().replace("(X-GM-MSGID", "X-GM-MSGID")
-            # print(line1)
-            splits1 = line1.split(" ")
-            for z in range(1, len(splits1) - 1, 2):
-                if splits1[z] == "X-GM-MSGID":
-                    _msg["X-GM-MSGID"] = splits1[z + 1]
-                elif splits1[z] == "RFC822.SIZE":
-                    _msg["Size"] = int(splits1[z + 1])
-            # print(splits1)
-            line2 = d[1].decode()
-            # print(line2)
-            splits2 = line2.split("\r\n")
-            for s in splits2:
-                if s.startswith("To"):
-                    _msg["To"] = s.replace("To: ", "")
-                elif s.startswith("From"):
-                    _msg["From"] = s.replace("From: ", "")
-                elif s.startswith("Subject"):
-                    _msg["Subject"] = s.replace("Subject: ", "")
-                elif s.startswith("Date"):
-                    _msg["Date"] = s.replace("Date: ", "")
-            messages.append(_msg)
-
-    # Logout from the IMAP server
-    print(messages[0])
-    print(len(messages))
-    imap.logout()
-    return d, data, line1, line2, messages, resp, s, splits1, splits2, x, z
+def __(getEmailsFromServer):
+    rawGemailDF = getEmailsFromServer()
+    return rawGemailDF,
 
 
 @app.cell
@@ -96,17 +103,80 @@ def __():
 
 
 @app.cell
-def __(messages, pd):
-    df = pd.DataFrame(messages)
-    df["Date"] = pd.to_datetime(df["Date"], errors="coerce")
-    # Save the DataFrame to a CSV file
-    df.to_csv("emails.csv", index=False)
+def __(mo):
+    mo.md(r"Find broken To addresse?")
+    return
+
+
+@app.cell
+def __(rawGemailDF):
+    x = 0
+    for _, row in rawGemailDF.iterrows():
+        if not isinstance(row["To"], (str, bytes)):
+            # print(row)
+            x += 1
+    print(x)
+    return row, x
+
+
+@app.cell
+def __():
+    import re
+
+
+    def extract_email(to_field):
+        if not isinstance(to_field, (str, bytes)):
+            # print(to_field)
+            return to_field
+        match = re.search(r"[\w\.-]+@[\w\.-]+", to_field)
+        return match.group(0) if match else to_field
+    return extract_email, re
+
+
+@app.cell
+def __(extract_email, pd, rawGemailDF):
+    import os
+
+
+    def emailsDF():
+        if os.path.exists("emails.csv"):
+            _df = pd.read_csv("emails.csv")
+        else:
+            _df = rawGemailDF.copy()
+            _df["To"] = _df["To"].apply(extract_email)
+            _df["From"] = _df["From"].apply(extract_email)
+            _df["Size"] = _df["Size"].apply(lambda x: round(x / 1024 / 1024,4))
+            _df.to_csv("emails.csv", index=False)
+
+        return _df
+    return emailsDF, os
+
+
+@app.cell
+def __(emailsDF, mo):
+    df = emailsDF()
+    df = df.dropna()
+    mo.ui.dataframe(df, page_size=400)
     return df,
 
 
 @app.cell
 def __(df, mo):
-    mo.ui.dataframe(df, page_size=400)
+    df_next = df.copy()
+    df_next = df_next[["To", "From", "Size"]]
+    df_next = df_next.groupby(["To","From"]).sum()
+    df_next.sort_values(by="Size", ascending=False)
+    mo.ui.dataframe(df_next, page_size=400)
+    return df_next,
+
+
+@app.cell
+def __():
+    return
+
+
+@app.cell
+def __():
     return
 
 
